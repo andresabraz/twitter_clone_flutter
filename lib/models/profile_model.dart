df727d76dcbef797a6c92c0ccddf165c603a2a2f
Andresa Braz <andresabraz@gmail.com>
2021-04-10 09:54:44 -0300

    Putting bio on profile page

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -2,10 +2,16 @@ class ProfileModel {
   String name;
   String picture;
   String nickname;
+  String bio;
 
   ProfileModel({
     required this.name,
     required this.picture,
     required this.nickname,
+    required this.bio,
   });
+
+  factory ProfileModel.empty() {
+    return ProfileModel(name: "", picture: "", nickname: "", bio: "");
+  }
 }
